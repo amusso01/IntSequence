85f888ff3fa0aa191e5cee11ba0f022524362e46
amusso01 <eandram@hotmail.it>
2017-01-29 20:46:03 +0000

    Add files via upload

--- IntSequence.java
+++ IntSequence.java
@@ -19,41 +19,38 @@ public class IntSequence
         long range;
         do{ //main loop of the program to run at least 1
             System.out.println("Please enter a whole number. 0 to quit");
-            boolean isInt = scan.hasNextLong();//call a Scanner method hasNextInt return true if a long has been entered
-            if(isInt){
-               input= scan.nextLong();
-               if(counter==0&&input==0){
-                   System.out.println("You must enter at least one number");
-                }else{
-                    if(counter==0){
+            boolean isInt = scan.hasNextLong();//call a Scanner method hasNextInt return true if a long has been entered            
+            input=scan.nextLong();
+                if(isInt){
+                    if(counter==0&&input==0){
+                        input=1;
+                        System.out.println("You must enter at least one value");
+                    }else if(counter==0){
                         largest=input;
                         smallest=input;
-                    }else{
+                        sum=input;
+                        counter++;
+                    }else if(input!=0){
                         if(input>largest){
                             largest=input;
                         }
                         if(input<smallest){
                             smallest=input;
                         }
+                        sum+=input;
+                        counter++;
                     }
-                    sum+= input;
-                    counter++;
-                    if(counter==1){System.out.println("number");}else{System.out.println("numbers");}
+                }else{
+                    System.out.println("The value entered is not a whole number.");
                 }
-               
-            }else{
-                System.out.println("The input is not a whole number");
-                input=scan.nextLong();
-                counter++;
-            }
         }while(input!=0);
         range=largest-smallest;
         average=sum/counter;
         System.out.println("The number of values entered is: "+counter);
-        System.out.println("So far you have entered "+sum);
-        System.out.println("So far you have entered "+smallest);
-        System.out.println("So far you have entered "+largest);
-        System.out.println("So far you have entered "+range);
-        System.out.println("So far you have entered "+average);
+        System.out.println("The sum of the values is "+sum);
+        System.out.println("The smallest value is "+smallest);
+        System.out.println("The largest value is "+largest);
+        System.out.println("The range of values is "+range);
+        System.out.println("The average of the values is "+average);
     }
 }
